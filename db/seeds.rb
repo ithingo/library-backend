e8b25b043ef01ad640785fcae6f2fccf244c4464
Vlad Borodaev <nikoskefali@gmail.com>
2018-11-18 00:39:27 +0400

    initial commit, add basic token auth

---
+++ db/seeds.rb
@@ -0,0 +1,2 @@
+User.create(email: 'test@test.ru', nickname: 'test', name: 'Test Test', password: "test")
+User.create(email: 'user@example.com', nickname: 'UOne', name: 'User One', password: "monkey67")
